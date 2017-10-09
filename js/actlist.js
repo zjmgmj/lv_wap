5793d9f9bbda75ded57290161526c4991f587952
zhjm <943257156@qq.com>
2017-10-09 14:04:33 +0800

    滚动加载

--- js/actlist.js
+++ js/actlist.js
@@ -22,6 +22,27 @@ $(document).ready(function() {
 		month = new Date().getMonth() + 1;
 		getGoodsNum(year + "-" + month + "-" + new Date().getDate());
 	}
+	
+	
+	
+	var temp_html='<div class="actlist_con"><a href="#"><div class="actlist_pic">'+
+						'<span class="adimg"><img src="images/actlist2.jpg" /></span>'+
+						'<span class="act_saled">累计已售89份</span>'+
+						'<div class="act_status_yes"><span class="act_price">'+
+						   '<img src="images/money2.png" />8900<i class="small_font">起</i></span>'+
+							'<span class="act_back">拼团返¥300</span>'+
+						'</div>'+
+						'<div class="act_status_no" style="display: none;">已结束</div>'+
+					'</div>'+
+					'<div class="actlist_tit">'+
+						'<h1>【 麦淘三周年庆VIP邀请函】仅限100名，观众</h1>'+
+						'<p>VIP坐席、后台探班、优先进场、精美礼物，尊享四大特权</p>'+
+					'</div>'+
+					'<div class="actlist_icon">'+
+						'<span>儿童剧</span><span>上海市浦东新区</span><span>3-18岁</span>'+
+					'</div></a></div>'
+	//滚动加载
+	lz_load($("#actlist_content"),temp_html);
 });
 
 var countArray = [];

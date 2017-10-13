aee4de69ac2dc125672272d6ae8c6694e15574d2
Administrator <Administrator@192.168.0.100>
2017-10-13 20:37:55 +0800

    20171013

--- js/info.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('.contract_info_c_list').click(function() {
-		$(this).next('.contract_info_c_toggle').toggle(100);
-	});
-	$(".check_radio").click(function(){
-		$(this).parents('.classify').find('.check_radio').removeClass("checked");
-		$(this).addClass("checked");
-	});
-});

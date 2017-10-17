2f3dc57476829258eb568f4e82226d71cd37defb
zhjm <943257156@qq.com>
2017-10-17 14:15:26 +0800

    20171017

---
+++ js/improveinfo.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	$("#input1").shijian({
+		startYear: 1989,
+		val: 1989,
+		endYear: 2017,
+
+		Hour: false, //是否显示小时
+		Minute: false, //是否显分钟
+		okfun: function(sjObj) {
+			age('#input1', sjObj.opt.yyyy);
+		}
+	});
+
+	//年龄计算
+	function age(obj, endYear) {
+		var today_date = new Date();
+		var today_year = today_date.getUTCFullYear(),
+			today_month = today_date.getUTCMonth() + 1,
+			today_day = today_date.getDate();
+		var input_val = today_year - endYear;
+		$(obj).val(input_val);
+	}
+
+	$('.choose_age_add').click(function() {
+		var html = '<div class="choose-age"><p></p><input type="text" value="孩子生日" id="input2" /></div>'
+		$('.info-age').append(html);
+		$(this).hide();
+		$("#input2").shijian({
+			startYear: 1989,
+			val: 1989,
+			endYear: 2017,
+
+			Hour: false, //是否显示小时
+			Minute: false, //是否显分钟
+			okfun: function(sjObj) {
+				age('#input2', sjObj.opt.yyyy);
+			}
+		});
+	});
+
+});
